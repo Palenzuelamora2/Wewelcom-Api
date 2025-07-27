4628c91ede40e54dbfc11279a991a6ab12cc9e1d
Palenzuelamora2 <daniel.palenzuela@juanxxiii.net>
2025-07-27 21:45:28 +0200

    fix: Final correction for TrustProxies middleware

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies;
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, \Closure $next)
+    {
+        // Configura el proxy para confiar en todos los proxies (para Railway)
+        // Railway utiliza un rango de IPs dinámico, por lo que confiar en todos
+        // los proxies es la forma más sencilla.
+        // En un entorno de producción más controlado, podrías especificar IPs exactas.
+        $this->proxies = '*'; 
+
+        return parent::handle($request, $next);
+    }
+}
+
