d4b48adb709a880ce1320730ceaebf95e17eb610
Palenzuelamora2 <daniel.palenzuela@juanxxiii.net>
2025-07-29 17:27:07 +0200

    Nuevo commit

--- config/services.php
+++ config/services.php
@@ -15,7 +15,7 @@ return [
     */
     
     //Añadimos esta linea para poder llamarla en el VerificarApiKey.php
-    'api_key' => env('API_KEY_FRONTEND'),
+    'api_key' => env('API_KEY'),
 
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
